932563679f9560e2141d12ad2a1824620fa1c490
Ameer67 <ameerzaita9@gmail.com>
2018-08-11 08:37:34 -0500

    added events.php, members.php, and police.php

--- index.php
+++ index.php
@@ -52,9 +52,8 @@
     
 
     <footer>
-        <h5>Current date and time: <span class="dateandtime">
-        <?php $date = date('F d, Y | g:i a'); echo $date ?>
-</span> [Chicago | Central Time]</h5>
+        <h5><span class="dateandtime">
+        <?php echo date('F d, Y | g:i a');?></span> [Chicago | Central Time]</h5>
         <h6>The Racers &amp; Chasers Club created by Ameer67. None of the text on this website may be used without permission.</h6>
     </footer>
 
