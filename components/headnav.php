08fd666d24b825cc0166816adf9d9ad5e290ebca
Ameer67 <ameerzaita9@gmail.com>
2018-08-15 18:00:42 -0500

    moved logo and nav to center of header and applied blur and brightness to header img

--- components/headnav.php
+++ components/headnav.php
@@ -17,19 +17,21 @@
 </head>
 <?php include("dbsettings.php") ?>
 <header>
-    <div class="logocontainer">
-        <a href="index.php"><img class="logo" src="img/TR&CC_main_shadow.png"></a>
+    <div class="logonavcontainer">
+        <div class="logocontainer">
+            <a href="index.php"><img class="logo" src="img/TR&amp;CC_main_shadow.png"></a>
+        </div>
+        <nav>
+            <ul>
+            <li><a href="index.php"><span>home</span></a></li>
+            <li><a href="sessions.php"><span>sessions</span></a></li>
+            <li><a href="rules.php"><span>rules</span></a></li>
+            <li><a href="members.php"><span>members</span></a></li>
+            <li><a href="police.php"><span>police</span></a></li>
+            <li><a href="#"><span>thread</span></a></li>
+            </ul>
+        </nav>
     </div>
-    <nav>
-        <ul>
-        <li><a href="index.php"><span>home</span></a></li>
-        <li><a href="sessions.php"><span>sessions</span></a></li>
-        <li><a href="rules.php"><span>rules</span></a></li>
-        <li><a href="members.php"><span>members</span></a></li>
-        <li><a href="police.php"><span>police</span></a></li>
-        <li><a href="#"><span>thread</span></a></li>
-        </ul>
-    </nav>
 </header>
 <script src="scripts/header.js"></script>
 <body>
