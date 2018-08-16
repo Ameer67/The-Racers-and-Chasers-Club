16ea91b91c3627a1a256714c1019713b9e046221
Ameer67 <ameerzaita9@gmail.com>
2018-08-15 22:30:10 -0500

    added 404 page

---
+++ error404.php
@@ -0,0 +1,9 @@
+<?php include("components/headnav.php"); ?>
+<div class="intro">
+    <div class="borderdown"></div>
+    <h1>You lost? [404]</h1>
+    <p>The page you are looking for does not exist, or you have incorrectly typed it out.
+    </p>
+    
+</div>
+<?php include("components/footer.php"); ?>
