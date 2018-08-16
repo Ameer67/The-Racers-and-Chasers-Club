f7e4eb580e70344d09b018afa1a9208c21afc8fe
Ameer67 <ameerzaita9@gmail.com>
2018-08-16 12:22:27 -0500

    added police logo and description to police page

---
+++ police.php
@@ -0,0 +1,44 @@
+<?php include("components/headnav.php"); ?>
+<div class="intro">
+    <div class="borderdown"></div>
+    <div class="policelogocontainer">
+        <img src="img/LothianPoliceBlack.png" alt="Lothian Police Logo">
+    </div>
+    <p>Formed in 2018, the new Lothian Police Service protects the Lothian region and surrounding communities from the recent surge in illegal street racing.
+    </p>
+    
+</div>
+<div class="infobox box1">
+    <div class="description">
+        <h3>Fun</h3>
+        <p>There are two sides participating in our sessions:
+        the cops and the racers. Members are given the 
+        oppurtunity to wreak havoc as a criminal or 
+        enforce the laws with other officers.</p>
+    </div>
+    <div class="border"></div>
+
+</div>
+<div class="infobox box2">
+    <div class="description">
+        <h3>Realistic</h3>
+        <p>In this club, we try to keep things as 
+        close to the real world as possible without
+        interfering with the level of fun.</p>
+    </div>
+    
+    <div class="infopic">
+        
+    </div>
+    <div class="border"></div>
+
+
+</div>
+<div class="infobox box3">
+    <div class="description">
+        <h3>Democratic</h3>
+        <p>ddsd</p>
+    </div>
+    <div class="border"></div>
+</div>
+<?php include("components/footer.php"); ?>
