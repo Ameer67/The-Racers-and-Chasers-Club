2cbed2b35e64a6e72b335006668d45979ad249f6
Ameer67 <ameerzaita9@gmail.com>
2018-08-17 17:10:22 -0500

    removed borderdown from 404 page

--- error404.php
+++ error404.php
@@ -1,6 +1,5 @@
 <?php include("components/headnav.php"); ?>
 <div class="intro">
-    <div class="borderdown"></div>
     <h1>You lost? [404]</h1>
     <p>The page you are looking for does not exist, or you have incorrectly typed it out.
     </p>
