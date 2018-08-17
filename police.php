f366c4c5e91d9728c6a1b8be3a5476a5347cee0e
Ameer67 <ameerzaita9@gmail.com>
2018-08-16 22:35:52 -0500

    made h3 orange and h4 blue

--- police.php
+++ police.php
@@ -11,8 +11,24 @@
             <h3>Catching the criminals</h3>
             <p>The goal of the LPS is to bust racers as fast and as safely as possible. This is done using a variety of maneuvers and tactics. The stages of a pursuit are listed below.</p>
         </div>
-        <div class="infobottom">
-            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div class="infobottom quadgrid">
+            <div>
+                <h4>1. Patrolling</h4>
+                <p>All on-duty police officers will be on the lookout for anyone causing trouble. Things like going above the speed limit, causing damage, driving like a drunkard, etc. will attract the police to a racer. Police are not allowed to continually follow a racer around unless they are doing something suspiscous.</p>
+            </div>
+            <div>
+                <h4>2. Initiating the pursuit</h4>
+                <p>Once the officer sees an offender, they will attempt to pull them over by driving behind their vehicle and honking their horn a few times. Officers should give racers a few seconds to pull over before starting a chase.</p>
+            </div>
+            <div>
+                <h4>3. Chasing the racer</h4>
+                <p>To bust a racer, the police must surrond the him/her for 10 seconds. If the pursuit lasted 10 or more minutes and/or the SWAT team was called in, the racer's car is impounded. Otherwise, the racer gets a second try. The second try (if there is one) will always result in an impound at the end.</p>
+            </div>
+            <div>
+                <h4>4. Busting the racer or calling of the pursuit</h4>
+                <p>To bust a racer, the police must surrond the him/her for 10 seconds. If the pursuit lasted 10 or more minutes and/or the SWAT team was called in, the racer's car is impounded. Otherwise, the racer gets a second try. The second try (if there is one) will always result in an impound at the end.</p>
+            </div>
+        
         </div>
     </div>
     
