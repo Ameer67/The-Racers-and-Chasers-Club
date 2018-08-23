fb0760410e6cc847673bcbfbd6e416bdc023512a
Ameer67 <ameerzaita9@gmail.com>
2018-08-23 16:38:27 -0500

    added members template

---
+++ components/memberlist.php
@@ -0,0 +1,23 @@
+<?php 
+    $membersql = "SELECT * FROM members";
+    $memberquery = mysqli_query($databaseConnection, $membersql);
+
+    while($row = mysqli_fetch_array($memberquery)){
+        $gtp_name = $row["gtp_name"];
+        $xbox_name = $row["xbox_name"];
+        $join_date = date("n/j/Y", strtotime($row["join_date"]));
+        $gtp_avatar = $row["gtp_avatar"];
+        echo 
+        '<div class="member">
+                <div class="memberimg" >
+                    <img src="' . $gtp_avatar . '">
+                </div>
+                <div class="memberinfo">
+                    <h4>' . $gtp_name . '</h4>
+                    <p>' . $xbox_name . '</p>
+                    <span>Joined on ' . $join_date . '</span>
+                </div>
+                
+            </div>';
+    }
+?>
--- members.php
+++ members.php
@@ -18,37 +18,7 @@
             </div>
         </div>
         <div class="infobottom pursuitgrid">
-            <div>
-                <div>
-                    <img src="https://www.gtplanet.net/forum/data/avatars/l/233/233077.jpg?1534537666">
-                </div>
-                <div>
-                    <h4>Ameer67</h4>
-                    <p>AmeerSixtySeven</p>
-                    <p>Joined on 8/19/2018</p>
-                </div>
-                
-            </div>
-            <div>
-                <h4>2. Initiating the pursuit</h4>
-                <p>Once the officer sees an offender, they will attempt to pull them over by driving behind their vehicle and honking their horn/siren a few times. Officers should give racers a few seconds to pull over before starting a chase.</p>
-            </div>
-            <div>
-                <h4>3. Chasing the racer</h4>
-                <p>If the offender does not stop their vehicle, the officer and offender are now in a pursuit. Whatever is needed to bust the racer will be done.</p>
-            </div>
-            <div>
-                <h4>4A. Busting the racer</h4>
-                <p>Police must surround the racer for at least 10 seconds to be considered caught. If police lose visual contact for 2 minutes, the racer has successfully evaded.</p>
-            </div>
-            <div>
-                <h4>4B. Racer first try bust</h4>
-                <p> If the chase lasted 10+ minutes, reached priority 1, and/or the SWAT team was called in, racers do NOT get a second try. Otherwise, move to 4C.</p>
-            </div>
-            <div>
-                <h4>4C. Racer second try bust (if applicable)</h4>
-                <p>The racer's turn is up, regardless of whether or not the conditions previously stated were met.</p>
-            </div>
+            <?php include("components/memberlist.php") ?>
         
         </div>
     </div>
