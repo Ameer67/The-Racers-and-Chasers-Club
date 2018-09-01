34be28726ba2f779ce8f44c80b6236b2b9e1acd9
Ameer67 <ameerzaita9@gmail.com>
2018-09-01 14:56:40 -0500

    began work on members application

--- members.php
+++ members.php
@@ -3,7 +3,7 @@
     <div>
         <h1>Members of the club</h1>
     </div>
-    <p>The Racers &amp; Chasers Club</p> 
+    <p>Take a look at who's in The Racers &amp; Chasers Club</p> 
 </div>
 <div class="infobox noimg">
     <div>
@@ -25,5 +25,25 @@
     
     <div class="infoimg"></div>
     <div class="border"></div>
+</div>
+<div>
+    <h3>Join the club</h3>
+    <p>Like what you see and interested in joining us? Please complete the steps below.</p>
+    <form action="">  
+        <label for="gtpaccount">
+    I have created a GTPlanet account <input type="checkbox" name="gtpaccount" id="gtpaccount">
+    </label>
+        <input type="checkbox" name="gtpaccount" id="">
+        <input type="checkbox" name="microphone" id="">
+        <select name="stealthunits" id=""></select>
+        I have created a GTPlanet account []
+        I have a microphone []
+        How many stealth units are allowed on-duty? [0,1,2,3,4]
+
+        Xbox Live Gamertag
+        Time zone
+        How did you discover this club?
+        Anything else you would like to share?
+    </form>
 </div>
 <?php include("components/footer.php"); ?>
