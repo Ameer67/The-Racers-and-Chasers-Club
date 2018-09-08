841d0f6e02c268e60e48a81992a9feb16d2c365d
Ameer67 <ameerzaita9@gmail.com>
2018-09-07 19:58:26 -0500

    added more info to open patrol

--- sessions.php
+++ sessions.php
@@ -13,13 +13,16 @@
         <div class="infobottom">
             <div>
                 <h4>Objective</h4>
-                <p>Racers: Roam around the streets causing havoc and avoid getting busted.</p>
+                <p><span>Racers:</span> Roam around the streets causing havoc and avoid getting busted.</p>
                 <p>Cops: Be on the lookout for anyone causing trouble and bust them immediately.</p>
             </div>
             <div>
-                <h4>Objective</h4>
-                <p>Racers: Roam around the streets causing havoc and avoid getting busted.</p>
-                <p>Cops: Be on the lookout for anyone causing trouble and bust them immediately.</p>
+                <h4>How it works</h4>
+                <p>One person is assigned to be a racer, and everyone else is a cop. (If there are 8+ players, two racers are allowed simultaneously.) See the <a href="police.php">"Catching the criminals"</a> section to see how pursuits work and when a racer is considered busted. Once a racer's turn is up, someone else is allowed to take their spot and the previous racer must switch to a cop. </p>
+            </div>
+            <div>
+                <h4>Specific rules/regulations</h4>
+                <p>One person is assigned to be a racer, and everyone else is a cop. (If there are 8+ players, two racers are allowed simultaneously.) See the <a href="police.php">"Catching the criminals"</a> section to see how pursuits work and when a racer is considered busted. Once a racer's turn is up, someone else is allowed to take their spot and the previous racer must switch to a cop. </p>
             </div>
         </div>
     </div>
