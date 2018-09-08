078457f94dff240fdcb3b0a29be93a207afd9157
Ameer67 <ameerzaita9@gmail.com>
2018-09-07 19:41:07 -0500

    began work on sessions page

--- sessions.php
+++ sessions.php
@@ -1,5 +1,32 @@
 <?php include("components/headnav.php"); ?>
-
+<div class="intro">
+    <div>
+        <h1>Game modes</h1>
+    </div>
+    <p>Formed in 2018, the new Lothian Police Service protects the Lothian region and surrounding communities from the recent surge in illegal street racing. Anyone with information about any suspicious activies should contact the LPS immediately.</p> 
+</div>
+<div class="infobox">
+    <div>
+        <div class="infotop">
+            <h3>Open Patrol</h3>
+        </div>
+        <div class="infobottom">
+            <div>
+                <h4>Objective</h4>
+                <p>Racers: Roam around the streets causing havoc and avoid getting busted.</p>
+                <p>Cops: Be on the lookout for anyone causing trouble and bust them immediately.</p>
+            </div>
+            <div>
+                <h4>Objective</h4>
+                <p>Racers: Roam around the streets causing havoc and avoid getting busted.</p>
+                <p>Cops: Be on the lookout for anyone causing trouble and bust them immediately.</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="infoimg"></div>
+    <div class="border"></div>
+</div>
 </section> 
 </body> 
 </html>
