5381012c4a57f5d45bb1ba197ec74fb0b9d0b3e3
Ameer67 <ameerzaita9@gmail.com>
2018-09-25 10:44:51 -0500

    added more info to index descriptions

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     the experiences of a police officer or a street racer while staying true to the following core values:
     fun, realistic, and democratic. Forza Horizon 4 is where club sessions are hosted
     (which are typically held on Fridays and weekends). 
-    Members are expected to join sessions approximately once
-    every two weeks or as much as possible. Take a look around the site and <a href="members.php">sign up</a> once you're ready!</p>
+    Members are expected to join sessions on a reasonable basis. Take a look around the site and <a href="members.php">sign up</a> once you're ready!</p>
 </div>
 <div class="infobox withimg">
     <div>
@@ -29,7 +28,7 @@
         <div class="infobottom">
             <p>In this club, we try to keep things as 
             close to the real world as possible without
-            interfering with the level of fun.</p>
+            interfering with the level of fun. (It' just a game in the end though, keep that in mind!)</p>
         </div>
     </div>
     
@@ -42,7 +41,7 @@
             <h3>Democratic</h3>
         </div>
         <div class="infobottom">
-            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>See something you don't like? Just bring it up in our <a href="https://www.gtplanet.net/forum/threads/the-racers-and-chasers-club-under-construction-for-fh4.351581/">GTPlanet thread</a> and we'll see what we can do. In TR&amp;CC, we want to make sure everyone's voice is heard.</p>
         </div>
     </div>
     
