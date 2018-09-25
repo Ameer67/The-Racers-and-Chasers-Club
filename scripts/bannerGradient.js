2b8fbfaf5e5e38a6a90ee37840c555a5c36e8de6
Ameer67 <ameerzaita9@gmail.com>
2018-09-24 22:53:26 -0500

    added infoimg and withimg css viewport styles

--- scripts/bannerGradient.js
+++ scripts/bannerGradient.js
@@ -3,11 +3,9 @@ var startingBG = getComputedStyle(banner[0]).backgroundImage;
 
 function gradientFunc() {
     if (window.innerWidth > 768) {
-        console.log("A");
-        banner[0].style.backgroundImage = "linear-gradient(to right, black, transparent 50%), " + startingBG;
+        banner[0].style.backgroundImage = "linear-gradient(to right, rgba(0,0,0,.85), transparent 50%), " + startingBG;
     }
     else {
-        console.log("B");
         banner[0].style.backgroundImage = "linear-gradient(to top, #0a1015, transparent 50%), " + startingBG;
     }
 }
