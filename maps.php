ba7f65abf7fc2b21e96a063f7de0128b8826eb25
Ameer67 <ameerzaita9@gmail.com>
2018-10-14 22:06:17 -0500

    added maps page and broadway

---
+++ maps.php
@@ -0,0 +1,48 @@
+<?php include("components/headnav.php"); ?>
+<div class="intro grid">
+    <div>
+        <h1>Maps</h1>
+    </div>
+    
+    <div class="description">
+        <p>The following maps are to be used in Open Patrol.</p>
+    </div>
+    
+
+</div>
+<div class="infobox">
+    <div>
+        <div class="infotop">
+            <h3>Ambleside</h3>
+            <p>The southernmost village, mostly residental with some offroading. Works best with 4-6 people.</p>
+        </div>
+        <div class="infobottom">
+            <div class="mapimgcontainer">
+                <img src="img/maps/ambleside.jpg" alt="ambleside">
+            </div>
+        </div>
+    </div>
+    
+    <div class="infoimg"></div>
+    <div class="border"></div>
+</div>
+<div class="infobox">
+    <div>
+        <div class="infotop">
+            <h3>Broadway</h3>
+            <p>The southernmost village, mostly residental with some offroading. Works best with 4-6 people.</p>
+        </div>
+        <div class="infobottom">
+            <div class="mapimgcontainer">
+                <img src="img/maps/broadway.jpg" alt="broadway">
+            </div>
+        </div>
+    </div>
+    
+    <div class="infoimg"></div>
+    <div class="border"></div>
+</div>
+
+</section> 
+</body> 
+</html>
