bd4e2ee75485afc7aaba83da2e31900d17ad5c6d
Ameer67 <ameerzaita9@gmail.com>
2018-10-17 00:26:41 -0500

    added list of banned engine swaps and muscle cars

--- rules.php
+++ rules.php
@@ -85,6 +85,50 @@
                     $rulequery = mysqli_query($databaseConnection, $carRule);
                     include("components/rulelist.php");
                 ?>
+                <li>Engine swaps originating from race cars are banned, which are as follows:
+                    <div class="policecarrestrictions" style="margin-top:.5em;">
+                        <div class="trigrid grid">
+                            <p>1.6L I4 - Turbo Rally (300hp)</p>
+                            <p>2.0L F4 - Turbo Rally (330hp)</p>
+                            <p>2.6L 4 Rotor - Racing (690hp)</p>
+                            <p>1.7L V12 - Racing (750hp)</p>
+                            <p>7.2L V8 - Racing (850hp)</p>
+                        </div>
+                    </div>
+                </li>
+                <li>Engine swaps are completely prohibited for the following vehicles (mostly D, C, B class muscle cars):
+                    <div class="policecarrestrictions" style="margin-top:.5em;">
+                        <div class="trigrid grid">
+                            <p>1965	Ford	Mustang GT Coupe</p>
+                            <p>1966	Chevrolet	Nova Super Sport</p>
+                            <p>1967	Chevrolet	Corvette [C2] Stingray 427</p>
+                            <p>1968	Dodge	Dart HEMI Super Stock</p>
+                            <p>1969	Chevrolet	Camaro  [Gen 1] SS Coupe</p>
+                            <p>1969	Chevrolet	Nova Super Sport 396</p>
+                            <p>1969	Dodge	Charger Daytona HEMI</p>
+                            <p>1969	Dodge	Charger R/T</p>
+                            <p>1969	Ford	Mustang Boss 302</p>
+                            <p>1969	Oldsmobile	Hurst/Olds 442</p>
+                            <p>1969	Pontiac	GTO Judge</p>
+                            <p>1970	Chevrolet	Camaro [Gen 2.1] Z28</p>
+                            <p>1970	Chevrolet	Chevelle Super Sport 454</p>
+                            <p>1970	Chevrolet	Corvette [C3] ZR-1</p>
+                            <p>1970	Dodge	Challenger R/T</p>
+                            <p>1971	AMC	Javelin-AMX</p>
+                            <p>1971	Ford	Mustang Mach 1</p>
+                            <p>1971	Plymouth	Cuda 426 Hemi</p>
+                            <p>1972	Chrysler	VH Valiant Charger R/T E49</p>
+                            <p>1972	Ford	Falcon XA GT-HO</p>
+                            <p>1973	Ford	XB Falcon GT</p>
+                            <p>1973	Holden	HQ Monaro GTS 350</p>
+                            <p>1973	Pontiac	Firebird Trans Am SD-455
+                            <p>1974	James Bond	AMC Hornet JB Edition</p>
+                            <p>1977	Holden	Torana A9X</p>
+                            <p>1977	Pontiac	Firebird Trans Am</p>
+                            <p>1979	Chevrolet	Camaro  [Gen 2.2] Z28</p>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
